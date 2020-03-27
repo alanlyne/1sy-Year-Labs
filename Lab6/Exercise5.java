166ac449c1d5fc50bbb27f7260857d596961dae8
alanlyne <alanlyne@outlook.com>
2020-03-27 10:40:38 +0000

    Exercise 5 done

--- Lab6/Exercise5.java
+++ Lab6/Exercise5.java
@@ -4,26 +4,35 @@ public class Exercise5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String [] hand = new String[5];
-        for(int i = 0; i < 5; i++){
+        String[] hand = new String[5];
+        for (int i = 0; i < 5; i++) {
             hand[i] = sc.nextLine();
         }
         fullHouseCheck(hand);
     }
 
-    public static void fullHouseCheck(String [] hand) {
+    public static void fullHouseCheck(String[] hand) {
+
         Arrays.sort(hand);
-        int temp = 0;
+        int distinct = 0;
+        int count = 1;
         String card = "";
-        for(int i = 0; i < 4; i++){
-            if(hand[i] == hand [i+1]){
-                temp++;
-                card = hand[i];
+        for (int i = 0; i < 5; i++) {
+            if (!hand[i].equals(card)) {
+                count = 1;
+                distinct++;
+            } else {
+                count++;
+            } if(count > 3){
+                break;
             }
-                if(temp > 1){
-                    System.out.println(temp + " of a kind: " + card);
-                
-            }
+            card = hand[i];
+        }
+        if(distinct > 2 || count > 3){
+            System.out.println("Not a full house");
+        } else{
+            System.out.println("3 of a kind is: " + hand[2]);
+            System.out.println("2 of a kind is: " + hand[4]);
         }
     }
 }
